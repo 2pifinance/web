3f2646572e27694154abf70c27a4856cfd505405
Batudo <batudo@2pi.finance>
2021-09-22 16:45:31 -0300

    New Spaceship

--- src/components/sections/footer.js
+++ src/components/sections/footer.js
@@ -11,7 +11,7 @@ const FooterSection = () => (
       </h2>
 
       <div className="row justify-content-center">
-        <div className="col-lg-9">
+        <div className="col-lg-9 col-xl-8 col-xxl-7">
           <Spaceship />
         </div>
       </div>
