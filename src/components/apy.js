846ed025ee4db17582217cee8c8411a20d29568d
Batudo <batudo@2pi.network>
2021-10-05 11:54:06 -0300

    Dynamic APY at last

--- src/components/apy.js
+++ src/components/apy.js
@@ -1,9 +1,31 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { ethers } from 'ethers'
+import { TwoPi } from '@2pi-network/js-sdk'
 
 const APY = () => {
+  const rpcUrl        = process.env.NEXT_PUBLIC_POLYGON_RPC_URL
+  const chainId       = 137
   const [apy, setApy] = useState()
 
-  setTimeout(() => { setApy(15) }, 3000)
+  useEffect(() => {
+    const tokens   = ['dai', 'usdt', 'usdc']
+    const provider = new ethers.providers.JsonRpcProvider(rpcUrl)
+    const twoPi    = new TwoPi(chainId, provider)
+    const vaults   = twoPi.getVaults().filter(
+      vault => tokens.includes(vault.token)
+    )
+
+    Promise.all(vaults.map(async vault => await vault.apy())).then(apys => {
+      const sum = apys.reduce((total, apy) => total + apy, 0)
+      const avg = (sum / apys.length) * 100
+
+      setApy(avg.toFixed(2))
+    }).catch(error => {
+      console.error(error)
+
+      setApy(15)
+    })
+  }, [rpcUrl])
 
   if (apy) {
     return (
