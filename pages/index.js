0221dfa783369d94f95cd2c6c253dd1326fd77d8
Matías Galí <7389958+matiasgali@users.noreply.github.com>
2021-11-08 10:32:05 -0300

    Add test app modal (#3)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Meta from '../src/components/meta'
+import Modal from '../src/components/modal'
 import MainSection from '../src/components/sections/main'
 import DevsSection from '../src/components/sections/devs'
 import FooterSection from '../src/components/sections/footer'
@@ -10,6 +11,7 @@ const Home = () => (
     <MainSection />
     <DevsSection />
     <FooterSection />
+    <Modal />
   </React.Fragment>
 )
 
